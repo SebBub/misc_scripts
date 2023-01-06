4539f34972d05a45d81e44b9f67819c6bcaa2b53
Sebastian Bubmann <sb1234@posteo.de>
2023-01-06 13:27:52 +0100

    Currently trying to overlay the polygon of Thuringia to the plot of ERA5 to understand where in the Thuringia Basin there may be gridpoints located between which (flat topo) one could easily bilinearly interpolate ERA5 gridpoint temperature records to compare with a station record as plausibility check, before using ERA5 dataset.
    Need to work on thesis now, maybe Felix can continue.

--- Gridded_Data/Plot_Grid.py
+++ Gridded_Data/Plot_Grid.py
@@ -1,101 +1,109 @@
-""" Plot grid of .nc file at a specified area.
+# TODO learn how to create issues and push them to GITHUB
+""" Plot grid of .nc files at a specified area.
 
-This script plots the grid of a .nc file on a map for visualisation
-around a specified points latitude and longitude with a specified +- offset
-in zonal and meridional direction. It's main aim is to quickly get a feeling
-for how the spatial dimensions of the area of interest and the grid spacing
-relate. Can also be used to see how many gridpoints fall within the area of
-interest."""
+This script overlays the grids of two .nc files on a map for visualisation
+around a point (lat/lon). Main objective  is to quickly get a feeling for
+how spatial dimensions (e.g. topography) and grid spacing relate. Can also
+be used to see how many gridpoints fall within the area of interest."""
 
 # Imports
 import os
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import numpy as np
-# import pandas as pd
 import xarray as xr
 import string
-
+import cartopy.io.shapereader as shpreader
 import matplotlib.pyplot as plt
 from pathlib import Path
 import matplotlib.ticker as mticker
-
 # set various parameters for the plots, adjust based on your Word document
 # formatting
 parameters = {"axes.titlesize": 11, "font.family": "Calibri", "font.size": 11,
               "axes.titleweight": "regular", "legend.fontsize": 11,
               "axes.labelsize": 11}
 plt.rcParams.update(parameters)
+del parameters
 
 # this defines where the map backgrounds reside
 os.environ["CARTOPY_USER_BACKGROUNDS"] = str(Path(os.getcwd()) /
                                              "Map_Background")
 alphabet = list(string.ascii_lowercase)  # string of all lowercase characters
 # point around which grids shall be visualised +- lat/lon - offset
-latitude = 51 ; longitude = 11  # Erfurt
+latitude = 51;
+longitude = 11  # Erfurt
 
-savefigs_to = Path(r"C:\Users\sb123\Documents\OneDrive\04_SS22\climThesis")
-data_path = Path(r"C:\Users\sb123\Documents\OneDrive\04_SS22\climThesis")
+savefigs_to = Path(r"C:\Users\sb123\Documents\OneDrive\04_SS22\climThesis"
+                   r"\Figures")
 
-# names of netCDF files for which grids shall be visualised
-datafiles = ["system_5_seasonal-monthly-single-levels.nc"]
+# Paths to netCDF files for which grids shall be visualised
+datafiles = [r"C:\Users\sb123\Documents\OneDrive\04_SS22\climThesis\Data"
+             r"\Observations\ERA5\ERA5.nc"]
 
-data_arrays = [] # fill empty list during following loop
+data_arrays = []  # fill empty list during following loop
 for datafile in datafiles:
-    data_arrays.append(xr.open_dataset(data_path / datafile))
+    data_arrays.append(xr.open_dataset(datafile))
 
-grids = [] # fill empty list during following loop
-for data_array in data_arrays:# fill list with lists of coordinates
-    try: # dimension names "latitude" and "longitude"
+grids = []  # fill empty list during following loop
+for data_array in data_arrays:  # fill list with lists of coordinates
+    try:  # dimension names "latitude" and "longitude"
         grids.append([np.round(data_array.latitude.values, 2), np.round(
             data_array.longitude.values, 2)])
-    except: # dimension names "lat" and "lon"
+    except:  # dimension names "lat" and "lon"
         grids.append([np.round(data_array.lat.values, 2), np.round(
             data_array.lon.values, 2)])
-number_grids = len(grids) #how many grids to visualise? equals len(datafiles)
+number_grids = len(grids)  # how many grids to visualise? equals len(datafiles)
 
 
 # %% Plotting
+def plot_grids(overlay=False, offset=5, figsize=(7, 7), shapefile=None):
+    if len(grids) == 1:
+        overlay = True
+    if len(grids) == 0:
+        print("Error! Grid list is empty!")
 
-def plot_grids(overlay=False, offset=4, figsize=(7, 7)):
     gridcolors = ["k", "r", "b", "g"]  # extend if needed to overlay more grids
+
+    offset = [longitude - offset, longitude + offset, latitude - offset,
+              latitude + offset]
+    if shapefile == None:
+        states_provinces = cfeature.NaturalEarthFeature(category="cultural",
+                                                        name="admin_1_states_provinces",
+                                                        scale="50m",
+                                                            edgecolor="red")
+    else:
+        reader = shpreader.Reader(shapefile)
+        THU = [entry for entry in reader.records() if entry.attributes[
+            "GEN"] == "Thueringen"][0]
+        shape_feature = cfeature.ShapelyFeature([THU.geometry],
+                                                ccrs.PlateCarree(),
+                                       facecolor=None, edgecolor='black',
+                                       lw=1)
     if not overlay:
         fig, ax = plt.subplots(ncols=number_grids, figsize=figsize,
                                subplot_kw={"projection": ccrs.PlateCarree()})
         fig.subplots_adjust(left=.05, right=.95, bottom=.2, top=.9, wspace=0.1,
                             hspace=.2)
-        # aax = fig.add_axes([0.4, 0.02, 0.2, .11], frame_on=True, xticks=[], yticks=[])
 
         for i, grid in enumerate(grids):
             # TODO add plot of topography later
-            # t_l = topo_large.plot.contourf(ax=ax[i], cmap=cm.terrain,
-            #                                add_colorbar=False, levels=100)
-            ax[i].set_title("Grid for file " + datafiles[i])
             gl = ax[i].gridlines(crs=ccrs.PlateCarree(), draw_labels=False,
-                                 linewidth=1, color="orange", alpha=0.5,
+                                 linewidth=1, color=gridcolors[i], alpha=0.5,
                                  linestyle="-")
             gl.xlocator = mticker.FixedLocator(grid[1])
             gl.ylocator = mticker.FixedLocator(grid[0])
-            ax[i].set_extent(
-                [longitude - offset, longitude + offset, latitude - offset,
-                 latitude + offset], ccrs.PlateCarree())
-            # ax[i]stock_img() # optional, better for larger offsets
-            # TODO add tick labels
+            ax[i].set_extent(offset, ccrs.PlateCarree())
             ax[i].add_feature(cfeature.BORDERS, linestyle="-")
-            ax[i].coastlines(
-                resolution="10m")  # these we do not need in Thuringia
-            # states_provinces = cfeature.NaturalEarthFeature(
-            #     category="cultural",
-            #     name="admin_1_states_provinces_lines",
-            #     scale="50m",
-            #     facecolor="none")
-            # ax[i].add_feature(states_provinces, edgecolor="gray")
-            ax[i].scatter(longitude, latitude, s=50, marker="x", color="r")
-            # TODO see above
-            # ax[i].scatter(gridpoints[i, 1], gridpoints[i, 0], s=50, marker="x",
-            #               color="orange")
+            ax[i].coastlines(resolution="50m")
+            if shapefile is None:
+                ax[i].add_feature(states_provinces, edgecolor="gray")
+            else:
+                ax[i].add_feature(shape_feature)
+            ax[i].scatter(longitude, latitude, s=50, marker="x", color="r",
+                          label='Erfurt')
             ax[i].annotate(alphabet[i], (0, -.08), xycoords="axes fraction",
                            weight="bold")
+
     else:
         fig, ax = plt.subplots(figsize=figsize,
                                subplot_kw={"projection": ccrs.PlateCarree()})
@@ -103,26 +111,28 @@ def plot_grids(overlay=False, offset=4, figsize=(7, 7)):
                             hspace=.2)
 
         for i, grid in enumerate(grids):
-            ax.set_title("Grids for files in " + str(datafiles))
             gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False,
                               linewidth=1, color=gridcolors[i], alpha=0.5,
                               linestyle="-")
             gl.xlocator = mticker.FixedLocator(grid[1])
             gl.ylocator = mticker.FixedLocator(grid[0])
-            ax.set_extent(
-                [longitude - offset, longitude + offset, latitude - offset,
-                 latitude + offset], ccrs.PlateCarree())
-            # ax[i].stock_img() # optional, better for larger offsets
-            # TODO add tick labels
-            ax.add_feature(cfeature.BORDERS, linestyle="-")
-            ax.coastlines(
-                resolution="10m")  # these we do not need in Thuringia
-            ax.scatter(longitude, latitude, s=50, marker="x", color="r")
+            ax.set_extent(offset)
+            ax.coastlines(resolution="50m")
+            if shapefile is None:
+                ax.add_feature(states_provinces, linestyle="-")
+            else:
+                ax.add_feature(shape_feature)
+            ax.scatter(longitude, latitude, s=50, marker="x", color="r",
+                       label='Erfurt')
+            ax.legend()
 
     fig.show()
-    fig.savefig(savefigs_to / "ERA5Orig_ERA5CoarseConservativeCDO.png",
-                dpi=500)
+    # fig.savefig(savefigs_to / "ERA5.png",
+    #             dpi=500)
 
 
 if __name__ == "__main__":
-    plot_grids(overlay=True)
+    shape = r"C:\Users\sb123\Documents\OneDrive\04_SS22\climThesis\Data\Geo" \
+            r"\vg250_12-31.utm32s.shape.ebenen\vg250_12-31.utm32s.shape" \
+            r".ebenen\vg250_ebenen_1231\VG250_LAN.shp"
+    plot_grids(overlay=True, shapefile=shape)
---
+++ Gridded_Data/README.md
@@ -0,0 +1,8 @@
+Important, you can find the names, which you can use to add Natural Earth 
+features in cartopy here (exclude the ".shp" at the end):
+https://github.com/nvkelso/natural-earth-vector/tree/master/
+Plug value in for name in
+https://scitools.org.uk/cartopy/docs/v0.14/matplotlib/feature_interface.html#cartopy.feature.NaturalEarthFeature
+
+Also see here for How to plot Shapefile data in Cartopy
+https://techoverflow.net/2021/04/25/how-to-plot-shapefile-data-in-cartopy/
